ba473aa92d834208486d7b7b13b2aec32bf20f3c
Sheri Gilley <sgilley@microsoft.com>
2016-12-20 11:26:21 -0600

    update gallery

---
+++ js/buttons.js
@@ -0,0 +1,14 @@
+// all button behaviors here
+$(function() {
+
+    $( "#toggleEyes" ).click(function() {
+        $('.eyes').toggle();
+    });
+    $( "#toggleNose" ).click(function() {
+        $('.nose').toggle();
+    });
+    $( "#toggleMouth" ).click(function() {
+        $('.mouth').toggle();
+    });
+
+});
